f6b0441cbffd9257342aa1c87c7e7249d4775abd
Matias Savela <msavela@gmail.com>
2015-01-11 23:36:08 +0200

    Added example on how to use Logger middleware.

---
+++ examples/logger/logger.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/msavela/deliver"
+	"github.com/msavela/deliver/middleware"
+	"net/http"
+	"log"
+)
+
+func main() {
+	d := deliver.New()
+
+	d.UseHandlerNext(middleware.NewLogger())
+
+	d.GET("/", func(res deliver.Response, req *deliver.Request) {
+		res.Status(http.StatusOK).Send(http.StatusText(http.StatusOK))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", d))
+}
